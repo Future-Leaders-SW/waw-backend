a8d6ac33629ab32cd01a6e62335000ebfe153395
Adrian Velasco <64821523+Luadveand@users.noreply.github.com>
2023-06-22 17:49:13 -0500

    feat: added new migration

---
+++ WAW.API/Migrations/20230622224721_AddedCvExtract.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WAW.API.Migrations
+{
+    public partial class AddedCvExtract : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "extract",
+                table: "cvs",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "extract",
+                table: "cvs");
+        }
+    }
+}
