3bb1f1630e22317ffe9eeef62781886f4502cf15
Adrian Velasco <64821523+Luadveand@users.noreply.github.com>
2023-06-22 01:48:45 -0500

    fix: migration added

---
+++ WAW.API/Migrations/20230622064617_AddedScoreToJobPostScore.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WAW.API.Migrations
+{
+    public partial class AddedScoreToJobPostScore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "score",
+                table: "job_post_scores",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "score",
+                table: "job_post_scores");
+        }
+    }
+}
